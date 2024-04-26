b41b0bef669a4d971080708645318a31847c740d
Артемий <artema121rus@mail.ru>
2024-04-26 18:52:06 +0300

    CombineReducer, Provider

---
+++ src/components/UserList.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+const UserList: React.FC = () => {
+    const store = useSelector((store) => store);
+    return <div></div>;
+};
+
+export default UserList;
