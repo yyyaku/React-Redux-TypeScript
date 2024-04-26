c6738a97b8b1075632aff238edd847fe78bc622a
Артемий <artema121rus@mail.ru>
2024-04-26 14:49:01 +0300

    Первый редюсер + типизация

---
+++ src/store/reducers/userReducer.ts
@@ -0,0 +1,27 @@
+import { applyMiddleware, createStore } from "redux";
+import { thunk } from "redux-thunk";
+import { UserAction, UserActionTypes, UserState } from "../../types/user";
+
+const initialState: UserState = {
+    users: [],
+    loading: false,
+    error: null,
+};
+
+export const userReducer = (
+    state = initialState,
+    action: UserAction
+): UserState => {
+    switch (action.type) {
+        case UserActionTypes.FETCH_USERS:
+            return { loading: true, error: null, users: [] };
+        case UserActionTypes.FETCH_USERS_SUCCESS:
+            return { loading: false, error: null, users: action.payload };
+        case UserActionTypes.FETCH_USERS_ERROR:
+            return { loading: false, error: action.payload, users: [] };
+        default:
+            return state;
+    }
+};
+
+const store = createStore(userReducer, applyMiddleware(thunk));
